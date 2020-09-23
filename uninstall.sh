a98e88cc98b566d95e286f29889264148a2d8f76
jrebort <s.yang.j507@gmail.com>
2020-09-23 17:32:57 +0800

    option modify

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# author:jrebort
+# date: 2020/9/23
+#
+# A uninstall script of Bksys program
+
+# root privilege
+if [ ${UID} -ne 0 ]; then
+    echo -e "\e[1;31mNon root user. Please run as root.\e[0m"
+    exit 1
+fi
+
+rm /usr/bin/bksys
